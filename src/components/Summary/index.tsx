6e45b09fcd7bec595b68ea4e5f87e6a9fc54a76c
eduardoalmeida45 <eduardoalmeida5547@gmail.com>
2022-04-10 14:41:37 -0300

    feat: calculating price summary

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -8,8 +8,23 @@ import totalIcon from '../../assets/total.svg'
 
 export function Summary() {
   const { transactions } = useContext(TransactionsContext)
-  
-  console.log(transactions);
+
+  const summary = transactions.reduce((acc, transaction) => {
+    if (transaction.type === 'deposit') {
+      acc.deposits += transaction.amount
+      acc.total += transaction.amount
+    } else {
+      acc.withdraws += transaction.amount
+      acc.total -= transaction.amount
+    }
+
+    return acc
+
+  }, {
+    deposits: 0,
+    withdraws: 0,
+    total: 0
+  })
 
   return (
     <Container>
@@ -18,7 +33,12 @@ export function Summary() {
           <p>Entradas</p>
           <img src={incomeIcon} alt="Entradas" />
         </header>
-        <strong>R$ 17.400,00</strong>
+        <strong>
+          {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).format(summary.deposits)}
+        </strong>
       </div>
 
       <div>
@@ -26,7 +46,12 @@ export function Summary() {
           <p>Saídas</p>
           <img src={outcomeIcon} alt="Saídas" />
         </header>
-        <strong>- R$ 1.400,00</strong>
+        <strong>
+          - {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).format(summary.withdraws)}
+        </strong>
       </div>
 
       <div className='highlight-background'>
@@ -34,7 +59,12 @@ export function Summary() {
           <p>Total</p>
           <img src={totalIcon} alt="Total" />
         </header>
-        <strong>R$ 16.900,00</strong>
+        <strong>
+          {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).format(summary.total)}
+        </strong>
       </div>
     </Container>
   )
