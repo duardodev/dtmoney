16f290e1b644ef4bf19cb359f61b3c66a4447e48
eduardoalmeida45 <eduardoalmeida5547@gmail.com>
2022-04-10 15:05:46 -0300

    feat: add alternative baseURL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'https://localhost:3000/api'
+  baseURL: 'https://dtmoney-sampaiocode.vercel.app/api'
+  // baseURL: 'https://localhost:3000/api'
 })
