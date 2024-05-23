b6a6666bc2c302deaf6ba74bf07f4f4879a51b6e
eduardoalmeida45 <eduardoalmeida5547@gmail.com>
2024-05-23 10:58:52 -0300

    chore: adjust baseURL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,5 @@
-import axios from 'axios'
+import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://dtmoney-sampaiocode.vercel.app/api'
-  // baseURL: 'https://localhost:3000/api'
-})
+  baseURL: '/api',
+});
